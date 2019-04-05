e4875bb0f27eb0809ec0a8575082c202df04d948
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-04-05 09:26:05 -0700

    Displays a count from 0 to user-inputted number, replacing anything with a 1 with Beep, anything with a 2 with Boop, and anything with a 3 with Im sorry Dave, I cant do that

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
+function replaceNumbers(input) {
+  var output = [];
 
+  // Loop from 0 to inputted number, replacing
+  // numbers with a 1 with Beep
+  // numbers with a 2 with Boop
+  // numbers with a 3 with I'm sorry, Dave. I'm afraid I can't do that.
+  // All other numbers, do not replace
+  for (var i = 0; i <= input; i++) {
+    var splitNum = i.toString().split("");
+    if (splitNum.includes("3")) {
+      output.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if (splitNum.includes("2")) {
+      output.push("Boop");
+    } else if (splitNum.includes("1")) {
+      output.push("Beep");
+    } else {
+      output.push(i);
+    }
+  }
+  return output.join(', ');
+}
 
 
 $(document).ready(function() {
@@ -10,13 +31,13 @@ $(document).ready(function() {
     if ($('input').val() === "" || parseInt($('input').val()) < 0 || isNaN(parseInt($('input').val()))) {
       alert ("Please enter a positive number or zero.");
     } else {
-      var splitNumber = input.split("");
+      var output = replaceNumbers(input);
+      console.log (output);
 
-      var output = input;
 
       $("#beepBoopForm").hide();
 
-      $(".input").text(input);
+//      $(".input").text(input);
       $(".output").text(output);
       $("#result").show();
     }
